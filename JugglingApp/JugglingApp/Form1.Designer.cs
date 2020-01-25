6735e8434fc131f26d93fcc71ab37ef2d11e6c97
mtotbagi <mtotbagi@users.noreply.github.com>
2020-01-25 18:35:59 +0100

    Drawing sequences

--- JugglingApp/JugglingApp/Form1.Designer.cs
+++ JugglingApp/JugglingApp/Form1.Designer.cs
@@ -31,15 +31,16 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.sequenceText = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(74, 47);
+            this.button1.Location = new System.Drawing.Point(47, 44);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(74, 50);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Create sequence";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -57,13 +58,22 @@
             this.sequenceText.Name = "sequenceText";
             this.sequenceText.Size = new System.Drawing.Size(100, 20);
             this.sequenceText.TabIndex = 3;
-            this.sequenceText.TextChanged += new System.EventHandler(this.sequenceText_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(402, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 37);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Site swap";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1155, 687);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.sequenceText);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
@@ -81,6 +91,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox sequenceText;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- JugglingApp/JugglingApp/Form1.cs
+++ JugglingApp/JugglingApp/Form1.cs
@@ -14,21 +14,23 @@ namespace JugglingApp
     {
 
         public Pen pen;
-        public Graphics formGraphics;
+        public Graphics gfx;
         public SolidBrush brush;
         public Color color;
-        public int screenHeight = Screen.PrimaryScreen.Bounds.Height;
-        public int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+        public int scrHeight = Screen.PrimaryScreen.Bounds.Height;
+        public int scrWidth = Screen.PrimaryScreen.Bounds.Width;
 
-        List<int> sequence;
+        List<int> currentSeq;
         int n;
+        int tmp;
 
         public Form1()
         {
             InitializeComponent();
-            sequence = new List<int>();
+            currentSeq = new List<int>();
             n = 15;
-            formGraphics = CreateGraphics();
+            tmp = 0;
+            gfx = CreateGraphics();
             Color color = Color.Black;
             pen = new Pen(color, 3);
             brush = new SolidBrush(color);
@@ -36,53 +38,70 @@ namespace JugglingApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            while(n <= 15)
+            ClearSequence();
+            currentSeq.Clear();
+            int tmpSeq = int.Parse(sequenceText.Text);
+            while(tmpSeq > 0)
             {
-                for (int i = 0; i < sequence.Count; i++)
-                {
-                    DrawThrow(n, sequence[i]);
-                }
+                currentSeq.Add(tmpSeq % 10);
+                tmpSeq = tmpSeq / 10;
             }
-            label2.Text = screenHeight.ToString();
+            currentSeq.Reverse();
+            DrawSequence(currentSeq);
         }
         
         private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawBeats();
+        }
+
+        void DrawThrow(int beat, int h)
+        {
+            gfx.DrawArc(pen, 100 * beat + 200 + 12, scrHeight * 8 / 10 - 75 * h + 12, 100 * h, 150 * h, 0, -180);
+        }
+
+        List<int> SiteSwap(List<int> sequence, int i, int j)
+        {
+            List<int> copy = sequence;
+            sequence[i] = copy[j] + j - i;
+            sequence[j] = copy[i] + i - j;
+            return sequence;
+        }
+
+        void DrawSequence(List<int> sequence)
+        {
+            tmp = 0;
+            while (tmp < 15)
+            {
+                for (int i = 0; i < currentSeq.Count; i++)
+                {
+                    if (tmp == 15) break;
+                    label2.Text += currentSeq[i] + "a";
+                    DrawThrow(tmp, currentSeq[i]);
+                    tmp++;
+                }
+            }
+        }
+
+        void DrawBeats()
         {
             for (int i = 0; i < n; i++)
             {
                 if (i % 2 == 0)
                 {
-                    formGraphics.DrawEllipse(pen, 100 * i + 200, screenHeight * 8 / 10, 25, 25);
+                    gfx.DrawEllipse(pen, 100 * i + 200, scrHeight * 8 / 10, 25, 25);
                 }
                 else
                 {
-                    formGraphics.FillEllipse(brush, 100 * i + 200, screenHeight * 8 / 10, 25, 25);
+                    gfx.FillEllipse(brush, 100 * i + 200, scrHeight * 8 / 10, 25, 25);
                 }
             }
         }
 
-        void DrawThrow(int beat, int h)
+        void ClearSequence()
         {
-            formGraphics.DrawArc(pen, 100 * beat + 200 + 12, screenHeight * 8 / 10 - 75 * h + 12, 100 * h, 150 * h, 0, -180);
-        }
-
-        private void sequenceText_TextChanged(object sender, EventArgs e)
-        {
-            TextBox seqText = sender as TextBox;
-            if (seqText.Text.Length > 0)
-            {
-                if (Char.IsNumber(seqText.Text[seqText.Text.Length - 1]))
-                {
-                    sequence.Add(seqText.Text[seqText.Text.Length - 1]);
-                    label2.Text = sequence[0].ToString();
-                }
-                else
-                {
-                    label2.Text = "Error, please write only numbers";
-                }
-            
-                label2.Text += seqText.Text[seqText.Text.Length - 1];
-            }
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+            gfx.FillRectangle(whiteBrush, 0, 0, 1920, 1080);
         }
     }
 }
