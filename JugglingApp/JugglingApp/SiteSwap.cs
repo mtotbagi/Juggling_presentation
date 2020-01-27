5a697bb77d9cbd73b2ca07dc66cc3073262aa3ec
mtotbagi <mtotbagi@users.noreply.github.com>
2020-01-27 18:53:13 +0100

    new form fixed

--- JugglingApp/JugglingApp/SiteSwap.cs
+++ JugglingApp/JugglingApp/SiteSwap.cs
@@ -12,6 +12,8 @@ namespace JugglingApp
 {
     public partial class SiteSwap : Form
     {
+        public int num1;
+        public int num2;
         public SiteSwap()
         {
             InitializeComponent();
@@ -19,7 +21,19 @@ namespace JugglingApp
 
         private void done_Click(object sender, EventArgs e)
         {
-            Hide();
+            try
+            {
+                num1 = int.Parse(textBox1.Text);
+                num2 = int.Parse(textBox2.Text);
+                Hide();
+            }
+            catch (FormatException)
+            {
+                string msg = "One of the numbers is incorrect!";
+                DialogResult result;
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                result = MessageBox.Show(msg,"Error",buttons);
+            }
         }
     }
 }
